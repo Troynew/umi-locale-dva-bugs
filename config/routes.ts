e7a4e9899203d919743f502fe25fc0d88d38ef57
yezh <yezh@yeastar.com>
2021-05-13 11:46:14 +0800

    【feat】init project

---
+++ config/routes.ts
@@ -0,0 +1,36 @@
+﻿export default [
+  {
+    path: '/',
+    component: '../layouts/BlankLayout',
+    routes: [
+      {
+        path: '/',
+        component: '../layouts/SecurityLayout',
+        routes: [
+          {
+            path: '/',
+            component: '../layouts/BasicLayout',
+            routes: [
+              {
+                path: '/',
+                redirect: '/welcome',
+              },
+              {
+                path: '/welcome',
+                name: 'welcome',
+                icon: 'smile',
+                component: './Welcome',
+              },
+            ],
+          },
+          {
+            component: './404',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    component: './404',
+  },
+];
